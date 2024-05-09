8b1a7e14c4b5fe90e2b6c5b3c3cf55828b0629a4
VoloDyzhak <volodyzhak@gmail.com>
2024-05-09 11:03:21 +0200

    clase arrays completa

--- practicaArrays/src/util/misArrays.java
+++ practicaArrays/src/util/misArrays.java
@@ -1,5 +1,64 @@
 package util;
 
+import java.lang.reflect.Array;
+import java.util.Iterator;
+
 public abstract class misArrays {
+	private int[] array;
+	
+
+	public misArrays(int[] array) {
+		
+		this.array= {1,2,3,4,5,6,7,8,9,10};
+		
+	}
+	
+	
+	
+	public int notaMedia() {
+		
+	int []array = {1,2,3,4,5,6,7,8,9,10} ;
+	
+    for (int i = 0; i < array.length; i++) {
+    
+		
+	} 
+
+     return 0;
+	}
+
+public int notaMinima(int[] array) {
+	
+
+ 
+    	        
+}
+    	        
+    	    	
+    	    
+    	
+
+		
+	
+	
+
+	
+
+
+
+public int notaMaxima(int[] array) {
+	
+	
+	int []arrayMinimo = {1,2,3,4,5,6,7,8,9,10} ;
+	
+
+    for (int i = 1; i < arrayMinimo.length; i++) {
+   	 int mayor = arrayMinimo[i] ;
+        if (arrayMinimo[i] >  mayor ) {
+            mayor = arrayMinimo[i];
+        }
+    }
+  return mayor;
+}   
 
 }
